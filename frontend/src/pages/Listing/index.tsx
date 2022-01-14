11271115e21853ff7334e20d5e5e1e12ae0e7b4a
phanetcrz <phanet@gmail.com>
2022-01-14 20:08:28 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,34 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
+    //forma Correta
+    const [pageNumber, setpageNumber] = useState(0);
 
-    //forma errada   *******Assistir vídeo java script promise https://www.youtube.com/watch?v=7KFduI7VLOM
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    useEffect(() =>{
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)  //desta forma ele faz a requisição duas vezes
         .then(response => {
             console.log(response.data);
+            const data = response.data as MoviePage;
+            setpageNumber(data.number);
         })
+    }, []);
+
+    //forma errada   *******Assistir vídeo java script promise https://www.youtube.com/watch?v=7KFduI7VLOM
+    // axios.get(`${BASE_URL}/movies?size=12&page=1`)  //desta forma ele faz a requisição duas vezes
+    //     .then(response => {
+    //         console.log(response.data);
+    //         const data = response.data as MoviePage;
+    //         setpageNumber(data.number);
+    //     })
 
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
 
             <div className="containet">
