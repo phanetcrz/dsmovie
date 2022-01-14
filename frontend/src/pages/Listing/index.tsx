c666a856910a4be9a83bcedd59e33f2b820e1b7a
phanetcrz <phanet@gmail.com>
2022-01-14 20:34:30 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -9,14 +9,26 @@ function Listing() {
     //forma Correta
     const [pageNumber, setpageNumber] = useState(0);
 
-    useEffect(() =>{
-        axios.get(`${BASE_URL}/movies?size=12&page=1`)  //desta forma ele faz a requisição duas vezes
-        .then(response => {
-            console.log(response.data);
-            const data = response.data as MoviePage;
-            setpageNumber(data.number);
-        })
-    }, []);
+    const [page, setPage] = useState<MoviePage>({    //--ESTADO QUE GUARDA A PÁGINA NA REQUISIÇÃO
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber},&sort=title`)  //desta forma ele faz a requisição duas vezes e o &sort=id garante que a busca será ordenada pelo titulo, pode usar o id e outros
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+                // console.log(response.data); teste
+            })
+    }, [pageNumber]);  //--quando mudar o pageNumber, vai executar o useEffect novamente 
 
     //forma errada   *******Assistir vídeo java script promise https://www.youtube.com/watch?v=7KFduI7VLOM
     // axios.get(`${BASE_URL}/movies?size=12&page=1`)  //desta forma ele faz a requisição duas vezes
@@ -28,26 +40,16 @@ function Listing() {
 
     return (
         <>
-        <p>{pageNumber}</p>
             <Pagination />
 
             <div className="containet">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
+                    {page.content.map(movie => (  //-- o content.map tem a mesma funcionalidade do For
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                    )}
                 </div>
             </div>
         </>
